e8ec95714834749c29df671868e74bb123b7cd0d
Oscar Caparros <ocaparrostortosa@gmail.com>
2020-09-14 13:19:28 +0200

    Added the flutter_localizations to the proyect

--- flutter_widgets/lib/main.dart
+++ flutter_widgets/lib/main.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/src/pages/alert_page.dart';
 import 'package:flutter_widgets/src/routes/routes.dart'; //Rutas de las aplicaciones
- 
+import 'package:flutter_localizations/flutter_localizations.dart'; //Idiomas
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''), // English, no country code
+        const Locale('es', 'ES'), // Hebrew, no country code
+        const Locale.fromSubtags(
+            languageCode: 'zh'), // Chinese *See Advanced Locales below*
+        // ... other locales the app supports
+      ],
       title: 'Flutter Widgets',
       //home: HomePagePages(),
       initialRoute: "/",
-      debugShowCheckedModeBanner: false,      
-      routes: getApplicationRoutes(), //Metodo para obtener las rutas de router.dart
-      onGenerateRoute: (settings) { //Metodo a ejecutar cuando se genere una ruta con nombre
-        print("Ruta llamada ${settings.name}"); 
-        return MaterialPageRoute (builder: (BuildContext context) => AlertPage());        
-        }, //Definimos las rutas para las paginas en la app
+      debugShowCheckedModeBanner: false,
+      routes:
+          getApplicationRoutes(), //Metodo para obtener las rutas de router.dart
+      onGenerateRoute: (settings) {
+        //Metodo a ejecutar cuando se genere una ruta con nombre
+        print("Ruta llamada ${settings.name}");
+        return MaterialPageRoute(
+            builder: (BuildContext context) => AlertPage());
+      }, //Definimos las rutas para las paginas en la app
     );
   }
 }
--- flutter_widgets/lib/src/pages/inputs_page.dart
+++ flutter_widgets/lib/src/pages/inputs_page.dart
@@ -10,6 +10,12 @@ class InputsPage extends StatefulWidget {
 class _InputsPageState extends State<InputsPage> {
 
   String _nombre = "";
+  String _email = "";
+  String _password = "";
+
+  String _fecha = "";
+  TextEditingController _inputFieldDateController = new TextEditingController(); //Controlador de la fecha en la caja de texto
+
   int _longitudMax = 20;
   int _longitudNombre = 20;
 
@@ -21,9 +27,15 @@ class _InputsPageState extends State<InputsPage> {
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
         children: <Widget>[          
-          _crearInput(),
+          _crearInput(),                   
           Divider(),
-          _crearPersona()
+          _crearEmail(),
+           Divider(),
+          _crearPassword(),
+           Divider(),
+           _crearFecha(context),
+           Divider(),
+          _crearPersona(),
         ],
       ),
     );
@@ -63,7 +75,92 @@ class _InputsPageState extends State<InputsPage> {
     return ListTile(
 
       title: Text("Nombre es: $_nombre"),
+      subtitle: Text("Email: $_email"),      
 
     );
   }
+
+
+  Widget _crearEmail(){
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(        
+        hintText: "Introduzca su email",
+        labelText: "E-mail",        
+        suffixIcon: Icon(Icons.alternate_email),
+        icon: Icon(Icons.email),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),        
+      ),
+      onChanged: (valor) => setState((){_email = valor;}),
+      
+
+    );
+  }
+
+  Widget _crearPassword(){
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(        
+        hintText: "Password",
+        labelText: "Password",        
+        suffixIcon: Icon(Icons.lock),
+        icon: Icon(Icons.lock_open),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),        
+      ),
+      onChanged: (valor) => setState((){_password = valor;}),
+      
+
+    );
+  }
+
+  Widget _crearFecha(BuildContext context){
+
+    return TextField(
+      enableInteractiveSelection: false,
+      controller: _inputFieldDateController, //Controlador de la caja de texto
+      decoration: InputDecoration(        
+        hintText: "Fecha nacimiento",
+        labelText: "Fecha",        
+        suffixIcon: Icon(Icons.perm_contact_calendar),
+        icon: Icon(Icons.calendar_today),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),        
+      ),  
+      onTap: (){
+
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _seleccionarFecha(context);
+
+      },
+
+    );
+
+  }
+
+  _seleccionarFecha(BuildContext context) async{
+
+    DateTime seleccionada = await showDatePicker(
+      context: context,
+      initialDate: new DateTime.now(),
+      firstDate: new DateTime(2000),
+      lastDate: new DateTime(2025),
+      locale: Locale('es', 'ES'), //Idioma
+    );
+
+    if(seleccionada != null){
+      setState(() {
+        _fecha = seleccionada.toString();
+        _inputFieldDateController.text = _fecha;
+      });
+    }
+
+  }
+
+
+
 }
